6ec2ad7dc44ce34020152e3ffc110fca4d3521a7
Eric Santos <djericsantos1@gmail.com>
2022-11-30 10:42:30 -0800

    Add files via upload

---
+++ vs_code.sh
@@ -0,0 +1,16 @@
+#!bin/bash/
+
+# Install CURL
+sudo apt-get install curl
+
+# Prerequists VS CODE
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# Install
+sudo apt-get update
+sudo apt install code
+sudo apt-get upgrade
