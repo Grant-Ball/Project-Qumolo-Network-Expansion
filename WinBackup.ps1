5c071fb9af75d61d15635c41f357f8adf86842f7
Scott C. Vipond <116005173+Scott-Vipond@users.noreply.github.com>
2022-12-01 15:55:54 -0800

    Create WinBackup.ps1

---
+++ WinBackup.ps1
@@ -0,0 +1,16 @@
+# Script: GigaRhino Win OS Backup
+# Author: Scott Vipond
+# Date: 1 Dec 22
+# Purpose: User OS Backup
+
+# Copies OS files from C:\ to E:\, will use Task Scheduler for frequency
+ROBOCOPY.EXE “C:\Windows” “E:\OS_Backup”
+
+#Script: GigaRhino Win OS Backup
+#Author: Scott Vipond
+# Date: 1 Dec 22
+# Purpose: User OS Backup
+
+# Copies OS files from C:\ to E:\, will use Task Scheduler for frequency
+# Need to change the User\ path to match the machine!
+ROBOCOPY.EXE “C:\User\UserName” “E:\User_Backup”
