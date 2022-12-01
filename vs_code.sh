10fa4e3f37dbf5e38eb8b1a9a33eee132fdf192b
Eric Santos <djericsantos1@gmail.com>
2022-12-01 11:29:29 -0800

    Update vs_code.sh

--- vs_code.sh
+++ vs_code.sh
@@ -11,6 +11,7 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code
 sudo apt install thunderbird
+sudo apt install borgbackup
 
 #Upgrades
 sudo apt-get upgrade
