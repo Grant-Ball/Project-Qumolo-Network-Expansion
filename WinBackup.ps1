6a7f3575499ec671b5d6b1434c0264f1b2281d82
Scott C. Vipond <116005173+Scott-Vipond@users.noreply.github.com>
2022-12-02 11:11:32 -0800

    Update WinBackup.ps1

--- WinBackup.ps1
+++ WinBackup.ps1
@@ -7,7 +7,7 @@
 # Purpose: User OS Backup
 
 # Copies OS files from C:\ to E:\, will use Task Scheduler for frequency
-ROBOCOPY.EXE “C:\Windows” “E:\OS_Backup”
+ROBOCOPY.EXE "C:\Windows" "E:\OS_Backup"
 
 #Script: GigaRhino Win User Backup
 #Author: Scott Vipond
@@ -16,4 +16,24 @@ ROBOCOPY.EXE “C:\Windows” “E:\OS_Backup”
 
 # Copies OS files from C:\ to E:\, will use Task Scheduler for frequency
 # Need to change the User\ path to match the machine!
-ROBOCOPY.EXE “C:\Users\UserName” “E:\User_Backup”
+ROBOCOPY.EXE "C:\Users\UserName" "E:\User_Backup"
+
+#Script: GigaRhino Win OS Backup
+#Author: Scott Vipond
+# Date: 2 Dec 22
+# Purpose: User OS Backup
+
+$Trigger= New-ScheduledTaskTrigger -Weekly -DaysofWeek Sunday -At 2am
+$User= “"NT AUTHORITY\SYSTEM"
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "ROBOCOPY.EXE C:\Windows 'E:\OS_Backup' /s > OSBackupLog.txt"
+Register-ScheduledTask -TaskName "OSBackup1" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
+
+#Script: GigaRhino Win User Backup
+#Author: Scott Vipond
+# Date: 2 Dec 22
+# Purpose: User Profile Backup
+
+$Trigger= New-ScheduledTaskTrigger -Daily -At 12am
+$User= "NT AUTHORITY\SYSTEM"
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "ROBOCOPY.EXE C:\Users\SaleRep 'E:\User_Backup' /s > UserLog.txt"
+Register-ScheduledTask -TaskName "UserBackup1" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
