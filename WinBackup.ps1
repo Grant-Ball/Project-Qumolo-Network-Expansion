65b74490fabc22098174e53a3482043ae70745a4
Scott C. Vipond <116005173+Scott-Vipond@users.noreply.github.com>
2022-12-02 05:05:56 -0800

    Update WinBackup.ps1

--- WinBackup.ps1
+++ WinBackup.ps1
@@ -6,7 +6,7 @@
 # Copies OS files from C:\ to E:\, will use Task Scheduler for frequency
 ROBOCOPY.EXE “C:\Windows” “E:\OS_Backup”
 
-#Script: GigaRhino Win OS Backup
+#Script: GigaRhino Win User Backup
 #Author: Scott Vipond
 # Date: 1 Dec 22
 # Purpose: User OS Backup
